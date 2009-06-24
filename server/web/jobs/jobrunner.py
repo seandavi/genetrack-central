53caee01f1c85716eeda9bd489550876e5b13747
Istvan Albert <istvan.albert@gmail.com>
2009-06-24 11:22:20 -0400

    more job related files

--- server/web/jobs/jobrunner.py
+++ server/web/jobs/jobrunner.py
@@ -1,6 +1,12 @@
+import time
 
-def start(data=data, ext=ext):
-    pass
+def execute_next(sleep=0):
+    
+    # this is used to start multiple jobs with cron (at every minute
+    # the
+    if sleep:
+        time.sleep(sleep)
+
 
 if __name__== '__main__':
     pass
