6f9a9fafa51672855e286c34019088faa2eb6fe0
Istvan Albert <istvan.albert@gmail.com>
2009-07-27 16:24:23 -0400

    cleaning up after reorganization

--- genetrack/server/urls/data_urls.py
+++ genetrack/server/urls/data_urls.py
@@ -1,22 +1,24 @@
 from django.conf.urls.defaults import patterns
 
-urlpatterns = patterns('genetrack.server.web.views',
+from genetrack.server.web.views import data
+
+urlpatterns = patterns('',
     #
     # data related views
     #    
-    (r'^edit/(?P<did>(.*))/$', 'data.edit'),
-    (r'^action/(?P<pid>(\d+))/$', 'data.action'),
-    (r'^view/(?P<did>(\d+))/$', 'data.view'),
-    (r'^upload/process/(?P<pid>(\d+))/$', 'data.upload_processor'),
-    (r'^upload/start/(?P<pid>(\d+))/$', 'data.upload_start'),
+    (r'^edit/(?P<did>(.*))/$', data.edit),
+    (r'^action/(?P<pid>(\d+))/$', data.action),
+    (r'^view/(?P<did>(\d+))/$', data.view),
+    (r'^upload/process/(?P<pid>(\d+))/$', data.upload_processor),
+    (r'^upload/start/(?P<pid>(\d+))/$', data.upload_start),
 
     # non applet version 
-    (r'^upload/simple/(?P<pid>(\d+))/$', 'data.upload_simple'),
+    (r'^upload/simple/(?P<pid>(\d+))/$', data.upload_simple),
 
-    (r'^download/(?P<did>(\d+))/$', 'data.download'),
-    (r'^result/get/(?P<rid>(\d+))/(?P<target>(\w+))/$', 'data.result_get'),
-    (r'^result/upload/(?P<did>(\d+))/$', 'data.result_upload'),
-    (r'^result/delete/(?P<rid>(\d+))/$', 'data.result_delete'),
+    (r'^download/(?P<did>(\d+))/$', data.download),
+    (r'^result/get/(?P<rid>(\d+))/(?P<target>(\w+))/$', data.result_get),
+    (r'^result/upload/(?P<did>(\d+))/$', data.result_upload),
+    (r'^result/delete/(?P<rid>(\d+))/$', data.result_delete),
 
     # the browser
     #(r'^view/(?P<did>(\d+))/$', 'liondb.genetrack.browser.view'),
